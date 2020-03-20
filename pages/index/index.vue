7c84b1d56a9ff1952addc8e9009724bd5cc12a8a
vbea <vbea@foxmail.com>
2020-03-21 02:09:41 +0800

    add

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,10 +1,12 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<view class="list-todo">
+			<view id="item.objectId" class="item-todo" hover-class="hover-item" v-for="item in todoList" v-bind:key="item.objectId">
+				<view class="item-title">{{item.content}}</view>
+				<view class="item-update">{{item.updatedAt}}</view>
+			</view>
 		</view>
-		<view @click="toAdd">Hello world</view>
+		<view class="btnAdd" @click="toAdd">+</view>
 	</view>
 </template>
 
@@ -14,14 +16,25 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				todoList: []
 			}
 		},
 		onLoad() {
-			//Bmob.initialize("5c718880a0a2aee46502557392e705fa", "0364c7");
-			//Bmob.debug(true)
+			Bmob.initialize("5783eb16beed4de9", "0364c7");
+			//Bmob.debug(true);
 			let a = 100;
-			console.log("onLoad", moment("").format("YYYY-MM-DD HH:mm"));
+			console.log("onLoad", moment().format("YYYY-MM-DD HH:mm"));
+		},
+		onShow:function(){
+			const query = Bmob.Query('list');
+			query.find().then(res => {
+			  console.log("查询", res)
+			  this.todoList = res
+			  console.log(this.todoList.length)
+			}).catch(err => {
+			  console.log(err)
+			})
 		},
 		methods: {
 			toAdd: function(e) {
@@ -33,22 +46,44 @@
 		}
 	}
 </script>
-
+<style lang="scss">
+	.btnAdd {
+		z-index: 20;
+		color: white;
+		width: 90rpx;
+		right: 36rpx;
+		bottom: 36rpx;
+		height: 90rpx;
+		display: flex;
+		position: fixed;
+		font-size: 55rpx;
+		align-items: center;
+		border-radius: 90rpx;
+		justify-content: center;
+		background: $color-accent;
+		box-shadow: 1px 2px 3px rgba($color: $color-accent, $alpha: .4);
+	}
+</style>
 <style>
 	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+		font-size: 30rpx;
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	
+	.item-todo {
+		padding: 15rpx 20rpx;
+	}
+	
+	.item-todo + .item-todo {
+		border-top: 1px solid #eee;
+	}
+	
+	.hover-item {
+		background: #e0e0e0;
+	}
+	
+	.item-update {
+		color: #C0C0C0;
+		font-size: 25rpx;
 	}
 
 	.text-area {
